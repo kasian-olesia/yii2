d8cf6c90242b894f3f2ed267cbd7dba389f0d5cd
kasianova olesia <kas.olesia@gmail.com>
2019-04-04 11:54:38 +0300

    Create Product.php
    lessen 1

---
+++ Yii2base/basic/models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * ContactForm is the model behind the contact form.
+ */
+class Product extends Model
+{
+    public $id;
+    public $name;
+    public $price;
+    public $category;
+
+
+}
