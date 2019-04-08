ccd4c00c728f78988ccdd379f266f2a2ab01fcdc
kasianova olesia <kas.olesia@gmail.com>
2019-04-08 14:51:18 +0300

    Create TestService.php
    lessen 2

---
+++ basic/components/TestService.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\components;
+use yii\base\Component;
+
+class TestService extends Component
+
+{
+    public $prop = 'default';
+    public  function  run()
+    {
+        return $this->prop;
+    }
+}
