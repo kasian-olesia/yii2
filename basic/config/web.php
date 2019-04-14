85abb92c73e01cc3ff98457ed8f4c65801d56d46
kasianova olesia <kas.olesia@gmail.com>
2019-04-14 14:21:03 +0300

    Create web.php

---
+++ basic/config/web.php
@@ -0,0 +1,87 @@
+<?php
+use app\components\TestService;
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'language' => 'ru',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'MgxEgkSz5smd3x0bCrJDvgbKcoLhNVBv',
+        ],
+        'cache' => [
+            'class' => yii\caching\FileCache::class,
+        ],
+        'user' => [
+            'identityClass' => app\models\User::class,
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => yii\swiftmailer\Mailer::class,
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => yii\log\FileTarget::class,
+                    'levels' => ['error', 'warning'],
+                ],
+                [
+                    'class' => yii\log\FileTarget::class,
+                    'levels' => ['info'],
+                    'categories' => ['pay'],
+                    'logFile' => "@runtime/logs/pay.log",
+                    'logVars' => [],
+                ],
+            ],
+        ],
+        'db' => $db,
+
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        'test' => [
+            'class' => \app\components\TestService::class,
+            //'prop' => '12345',
+            ]
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['*'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        'allowedIPs' => ['*'],
+    ];
+}
+
+return $config;
