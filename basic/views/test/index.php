b03d5be9abd07a16cb50c500bb0562690b985b02
kasianova olesia <kas.olesia@gmail.com>
2019-04-14 14:23:57 +0300

    Create index.php

---
+++ basic/views/test/index.php
@@ -0,0 +1,9 @@
+<?php
+/* @var $this yii\web\View */
+/* @xar $data string */
+/* @var $product \app\models\Product */
+
+?>
+
+<h1>Test</h1>
+<?= \yii\widgets\DetailView::widget(['model' => $product])  ?>
